8fa01c654f1524d0acb09a3ba803de99c16bba4d
Graeme Brough <gdbrough@blueyonder.co.uk>
2018-04-06 14:40:09 +0100

    adds collector can buys records when has enough cash

--- collector.js
+++ collector.js
@@ -4,5 +4,12 @@ var Collector = function(name, cash) {
   this.collection = [];
 };
 
+Collector.prototype.buyRecord = function(recordToBuy, storeToBuyFrom) {
+  if (recordToBuy.price <= this.cash){
+    storeToBuyFrom.sellRecord(recordToBuy);
+    this.cash -= recordToBuy.price;
+    this.collection.push(recordToBuy);
+  };
+};
 
 module.exports = Collector;
--- specs/collector_spec.js
+++ specs/collector_spec.js
@@ -11,6 +11,12 @@ describe("Collector", function(){
 
   beforeEach(function(){
     collector1 = new Collector("Allan", 100.00);
+    record1 = new Record("The Cult", "Electric", "rock", 12.99);
+    record2 = new Record("Velvet Revolver", "Contraband", "rock", 8.99);
+    record3 = new Record("AC/DC", "Highway to Hell", "rock", 9.99);
+    record4 = new Record("Eminem", "Encore", "rap", 4.99);
+    record5 = new Record("Guns 'n' Roses", "Appetite for Destruction", "rock", 11.99);
+    store1 = new Store("Record Shop", "Embra", [record1, record2, record3, record4, record5]);
   });
 
   it("should have a name", function(){
@@ -23,6 +29,19 @@ describe("Collector", function(){
 
   it("should start with an empty inventory", function(){
     assert.strictEqual(collector1.collection.length, 0);
+  });
+
+  it("should be able to buy records", function(){
+    collector1.buyRecord(record1, store1);
+    assert.strictEqual(collector1.cash, 87.01);
+    assert.deepEqual(collector1.collection, [record1]);
+  });
+
+  it("should not be able to buy records when does not have enough cash", function(){
+    collector2 = new Collector("Skint", 10.00);
+    collector2.buyRecord(record1, store1);
+    assert.strictEqual(collector2.cash, 10.00);
+    assert.strictEqual(collector2.collection.length, 0);
   });
 
 });
